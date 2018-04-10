8c5e95322270dfa2f3113b64468a8f5d6b28a024
Krsto Culafic <kculafic@gmail.com>
2018-04-09 17:06:26 -0700

    Wire event handler

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ $(document).ready(function() {
   // function dissemvowel() {
   //   console.log('*****test test test ********');
   // }
+  let bp = $('.blog-post').text();
+
+  function removeVowels(content){
+    let holder = [];
+
+  //   for (let i=0; i<content.length; i++) {
+  //     // console.log(content[i]);
+  //
+  //     if (content[i].toString() !== 'A' || 'a' || 'E' || 'e' || 'I' || 'i' || 'O' || 'o' || 'U' || 'u' || 'Y' || 'y') {
+  //       holder.push(content[i]);
+  //     }
+  //   }
+  //   console.log(holder);
+  //   let noMoreVowels = holder.join('');
+  //   console.log(noMoreVowels);
+  //   $('.blog-post').append(noMoreVowels);
+  // }
+
+
 
   $('#trigger').click(function() {
     console.log('***** button fired ********');
+
+    // console.log(bp);
+    removeVowels(bp);
   });
 
   // $('.blog-post').dissemvowel();
